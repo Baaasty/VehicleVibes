cd6a5053581e5824d24fef7c91380a0238a7d2f7
Baaasty <bmg180405@gmail.com>
2023-12-11 22:25:01 +0100

    Better ProfileView

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="profile-edit">
     <h1>Edit Profile</h1>
-    <Form :validation-schema="schema" class="profile-edit-form" @submit="saveProfile">
+    <form class="profile-edit-form" @submit="saveProfile">
       <div class="form-row">
         <div>
           <label for="firstname">Your name</label>
           <div class="input-container">
-            <Field id="firstname" name="firstname" type="text" autocomplete="off" />
-            <ErrorMessage name="firstname" class="error-message tiny" />
+            <input
+              id="firstname"
+              v-model="firstname"
+              v-bind="firstnameValue"
+              name="firstname"
+              type="text"
+              autocomplete="off"
+            />
+            <p v-if="errors.firstname" name="firstname" class="error-message tiny">{{ errors.firstname }}</p>
           </div>
         </div>
 
         <div>
-          <label for="birthday">Birthday</label>
+          <label for="birthdate">Birthdate</label>
           <div class="input-container">
-            <Field id="birthday" name="birthday" type="date" autocomplete="off" />
-            <ErrorMessage name="birthday" class="error-message tiny" />
+            <input
+              id="birthdate"
+              v-model="birthdate"
+              v-bind="birthdateValue"
+              name="birthdate"
+              type="date"
+              autocomplete="off"
+            />
+            <p v-if="errors.birthdate" name="birthdate" class="error-message tiny">{{ errors.birthdate }}</p>
           </div>
         </div>
       </div>
@@ -23,46 +37,68 @@
       <div>
         <label for="bio">Biography</label>
         <div class="input-container">
-          <Field id="bio" as="textarea" name="bio" type="textarea" autocomplete="off" />
-          <ErrorMessage name="bio" class="error-message tiny" />
+          <textarea
+            id="bio"
+            v-model="bio"
+            v-bind="bioValue"
+            name="bio"
+            as="textarea"
+            type="textarea"
+            autocomplete="off"
+          >
+          </textarea>
+          <p v-if="errors.bio" name="bio" class="error-message tiny">{{ errors.bio }}</p>
         </div>
       </div>
 
       <button type="submit" :disabled="loading">Save</button>
 
       <p v-if="message" class="error-message">{{ message }}</p>
-    </Form>
+    </form>
   </div>
+
+  <h1>{{ values }}</h1>
+  <h1>{{ errors }}</h1>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue';
+import { onBeforeMount } from 'vue';
 import ProfileService from '../services/profile.service';
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { useForm } from 'vee-validate';
 import * as yup from 'yup';
 
-const schema = yup.object().shape({
-  firstname: yup
-    .string()
-    .required('Name is required!')
-    .min(2, 'At least 2 characters!')
-    .max(28, 'Cannot be more than 28 characters!')
-    .matches(/^[a-zA-Z]+$/, 'Only letters are allowed!'),
-  birthday: yup
-    .date()
-    .required('Birthday is required!')
-    .min(new Date(1900, 0, 1), 'Must be after 1900!')
-    .max(new Date(new Date().setFullYear(new Date().getFullYear() - 18)), 'At least 18 years old!'),
-  bio: yup.string().max(500, 'Cannot be more than 500 characters!'),
+const { values, errors, handleSubmit, defineField } = useForm({
+  validationSchema: yup.object({
+    firstname: yup
+      .string()
+      .required('Name is required!')
+      .min(2, 'At least 2 characters!')
+      .max(28, 'Cannot be more than 28 characters!')
+      .matches(/^[a-zA-Z]+$/, 'Only letters are allowed!'),
+    birthdate: yup
+      .date()
+      .required('Birthdate is required!')
+      .min(new Date(1900, 0, 1), 'Must be after 1900!')
+      .max(new Date(new Date().setFullYear(new Date().getFullYear() - 18)), 'At least 18 years old!'),
+    bio: yup.string().max(500, 'Cannot be more than 500 characters!'),
+  }),
 });
 
-const loading = ref(false);
-const message = ref('');
+const [firstname, firstnameValue] = defineField('firstname', {
+  validateOnModelUpdate: false,
+});
+const [birthdate, birthdateValue] = defineField('birthdate', {
+  validateOnModelUpdate: false,
+});
+const [bio, bioValue] = defineField('bio');
 
 const loadProfile = async () => {
   ProfileService.getProfile().then(
     (res) => {
       console.log(res.data);
+      firstname.value = res.data.firstname;
+      birthdate.value = res.data.birthdate;
+      bio.value = res.data.bio;
     },
     (err) => {
       console.error('Error loading profile:', err);
@@ -70,10 +106,10 @@ const loadProfile = async () => {
   );
 };
 
-const saveProfile = async (profile) => {
-  console.log(profile);
+const saveProfile = handleSubmit((values) => {
+  console.log(values);
 
-  ProfileService.saveProfile(profile).then(
+  ProfileService.saveProfile(values).then(
     (res) => {
       console.log(res.data);
     },
@@ -81,7 +117,7 @@ const saveProfile = async (profile) => {
       console.error('Error updating profile:', err);
     }
   );
-};
+});
 
 onBeforeMount(() => {
   loadProfile();
@@ -109,7 +145,8 @@ onBeforeMount(() => {
 
     label {
       display: block;
-      color: #555; /* Softer label color */
+      color: #555;
+      margin-bottom: 10px;
     }
 
     .input-container {
@@ -120,7 +157,6 @@ onBeforeMount(() => {
       textarea {
         width: 100%;
         padding: 8px;
-        margin: 10px 0 0;
         box-sizing: border-box;
         border: 1px solid #ccc; /* Softer border color */
         border-radius: 4px;
@@ -133,11 +169,11 @@ onBeforeMount(() => {
       }
 
       .error-message {
-        color: #e44d4d; /* Softer error message color */
+        color: #e44d4d;
         font-size: 80%;
         position: absolute;
+        margin: 0;
         bottom: 0;
-        left: 0;
         transform: translateY(100%);
       }
     }
